ea3d602a73b95a97e9265231c61149908eb75cf2
jiwoo-hongb <jwhong48@gmail.com>
2024-10-18 20:41:26 +0900

    Main_GUI 프로토타입 수정
    1. 계산기 종류 라벨 위치 수정
    2. 결과값 라벨 수정 "JTextField -> JLabel"
    3. 버튼 위치 수정
    4. 프레임 레이아웃 수정 및 패널 여백 설정

--- src/gui/Main_Gui.java
+++ src/gui/Main_Gui.java
@@ -3,51 +3,74 @@ package gui;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ *
+// * @create
+ * - 10.15: 첫 Main_Gui 클래스 생성
+ * - 10.18: Main_Gui 프로토 타입 수정 / Javadocs 첫 생성
+ */
+
 public class Main_Gui extends JFrame {
-    JTextField textField;
+    JPanel main_Panel = new JPanel(); //메인 패널
+    JLabel result_Label = new JLabel();//결과값이 나올 곳
+    GridBagConstraints gbc = new GridBagConstraints();
 
     Main_Gui(){
         setTitle("계산기");
+        main_Panel.setLayout(new GridBagLayout());
+        gbc.fill = GridBagConstraints.BOTH;
 
-        setLayout(new BorderLayout());
+        gbc.weightx = 1;
+        gbc.weighty = 0.1;
+        gbc.gridy = 0;
         showNorth();
+
+        gbc.weightx = 1;
+        gbc.weighty = 0.2;
+        gbc.gridy = 1;
         showCenter();
+
+
+        gbc.weightx = 1;
+        gbc.weighty = 0.7;
+        gbc.gridy = 2;
         showSouth();
 
+        add(main_Panel);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(335, 540);
         setVisible(true);
     }
 
     void showNorth(){
-        JPanel panel = new JPanel();
+        JPanel panelStandard = new JPanel(new BorderLayout());
         JLabel label = new JLabel("표준");
 
-        panel.add(label);
-        add(panel, BorderLayout.NORTH);
+        panelStandard.add(label, BorderLayout.WEST);
+        panelStandard.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
+        main_Panel.add(panelStandard, gbc);
     }
 
     void showCenter(){
-        JPanel panel = new JPanel();
-        textField = new JTextField();
+        JPanel panelResult = new JPanel(new BorderLayout());
+        result_Label = new JLabel("0");
 
-        panel.add(textField);
-        add(panel, BorderLayout.CENTER);
+        panelResult.add(result_Label, BorderLayout.EAST);
+        panelResult.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
+        main_Panel.add(panelResult, gbc);
     }
 
-
     void showSouth(){
-        JPanel panel = new JPanel(new GridLayout(5, 4, 5, 5));
-        String[] cal_btn = {"CE", "C", "DEL", "÷", "7", "8", "9", "×", "4", "5", "6", "–", "1", "2", "3", "+", "±", "0", ".", "="};
+        JPanel panelBtn = new JPanel(new GridLayout(5, 4, 5, 5));
+        String[] cal_btn = {"AC", "CE", "←", "÷", "7", "8", "9", "×", "4", "5", "6", "–", "1", "2", "3", "+", "±", "0", ".", "="};
 
         for(int i = 0; i < 20; i++){
             JButton btn = new JButton(cal_btn[i]);
-            panel.add(btn);
+            panelBtn.add(btn);
         }
-        add(panel, BorderLayout.SOUTH);
+        panelBtn.setBorder(BorderFactory.createEmptyBorder(0, 5, 5, 5));
+        main_Panel.add(panelBtn, gbc);
     }
-
-
 
     public static void main(String[] args) {
         new Main_Gui();
